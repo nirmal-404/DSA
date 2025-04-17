60e11f72731d16e1d15671c3b9346f1dc15715ab
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-04-17 17:57:21 +0530

    quick sort

--- dsa/src/_16_quick_sort/Main.java
+++ dsa/src/_16_quick_sort/Main.java
@@ -1,6 +1,17 @@
 package _16_quick_sort;
 
 public class Main {
+	
+// 		Quick Sort = 	Moves smaller elements to the left of a pivot.
+//  					Recursively divides array into two partitions.
+
+//		Run-time complexity: 	Best case 		O(n log(n))
+//      	     				Average case 	O(n log(n))
+//           					Worst case 		O(n^2) 		if already sorted
+
+//		Space complexity: 		O(log(n)) 		due to recursion
+	
+	
 	public static void main(String[] args) {
 		  int[] array = {8, 2, 5, 3, 9, 4, 7, 6, 1};   
 		  
