cbfe934948bb46970cf8cc64bc41cc9cea0ace70
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-04-17 18:00:34 +0530

    tree traversal

--- dsa/src/_25_tree_traversal/Main.java
+++ dsa/src/_25_tree_traversal/Main.java
@@ -1,5 +1,7 @@
-package _25_tree_traversal;
 
-public class Main {
 
-}
+//	in-order traversal -> 
+
+// 	post-order traversal -> used to delete a tree from leaf to root
+
+//	pre-order traversal -> used to get a copy of a tree
